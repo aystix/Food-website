7cac857a41ec6146cf380fd1183ca6a8b8712ba1
adeyemiayomide <aystix2@gmail.com>
2023-06-07 02:08:23 -0700

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+let talkbtn = document.querySelector("#talk-btn");
+let textarea = document.querySelector("#txt-area");
+let sendbtn = document.querySelector("#send-btn");
+let form = document.querySelector("#form");
 
 menu.addEventListener("click", function(){
     
@@ -8,3 +12,15 @@ menu.addEventListener("click", function(){
 window.onscroll = () => {
     navbar.classList.remove("active");
 }
+
+talkbtn.addEventListener("click", function(){
+   textarea.style.display="block";
+   sendbtn.style.display="block";
+   talkbtn.style.display="none";
+   form.style.display="block";
+})
+form.addEventListener("submit", function(){
+    alert("your response has been received");
+    location.reload();
+    })
+    
